6a7e7b2626996459f1b952254fd9ca2c546bf1c2
Emilia Kirschenbaum <ekubokirs@gmail.com>
2014-01-21 13:03:50 -0800

    added missing semicolons

--- app/assets/javascripts/wishes.js
+++ app/assets/javascripts/wishes.js
@@ -7,7 +7,7 @@ $(function(){
 			$("#gw_newSassy").addClass("gw_active");
 			$("#gw_newNice").removeClass("gw_active");
 			$("#gw_newBlank").removeClass("gw_active");
-	})
+	});
 	$("#gw_newNice").on("click", function(evt){
 			$(".gw_newSassy").hide();
 			$(".gw_newNice").show();
@@ -16,7 +16,7 @@ $(function(){
 			$("#gw_newSassy").removeClass("gw_active");
 			$("#gw_newNice").addClass("gw_active");
 			$("#gw_newBlank").removeClass("gw_active");
-	})
+	});
 	$("#gw_newBlank").on("click", function(evt){
 			$(".gw_newSassy").hide();
 			$(".gw_newNice").hide();
@@ -25,6 +25,6 @@ $(function(){
 			$("#gw_newSassy").removeClass("gw_active");
 			$("#gw_newNice").removeClass("gw_active");
 			$("#gw_newBlank").addClass("gw_active");
-	})
+	});
 	return false;
 })
