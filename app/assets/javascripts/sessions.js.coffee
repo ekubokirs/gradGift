60f54a9c311fd4babd24348a2b720228c031570b
Emilia Kirschenbaum <ekubokirs@gmail.com>
2014-01-21 12:52:22 -0800

    finished facebook authentication and started photos

--- app/assets/javascripts/sessions.js.coffee
+++ app/assets/javascripts/sessions.js.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 10000,
+    pause_on_hover: true,
+    animation_speed: 500,
+    navigation_arrows: true,
+    bullets: true
+  }
+});
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,40 @@ class SessionsController < ApplicationController
 	def index
 		if current_user
 		 	@nav = "shared/friendNav"
+		 	@user = current_user
 		else
 			@nav = "shared/nonFriendNav"
 		end
 	end
 
-	def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_url
-  end
+	def new
+	  redirect_to '/auth/facebook'
+	end
+	# def create
+ #    user = User.from_omniauth(env["omniauth.auth"])
+ #    session[:user_id] = user.id
+ #    redirect_to root_url
+ #  end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url
-  end
+  def create
+	  auth = request.env["omniauth.auth"]
+	  user = User.where(:provider => auth['provider'],
+	                    :uid => auth['uid']).first || User.create_with_omniauth(auth)
+	  session[:user_id] = user.id
+	  redirect_to root_url, :notice => "Signed in!"
+	end
+
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_url
+  # end
   
+  def destroy
+	  reset_session
+	  redirect_to root_url, :notice => 'Signed out!'
+	end
+
+	def failure
+	  redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+	end
 end
