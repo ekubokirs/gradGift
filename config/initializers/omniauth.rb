5b862693e9753626342a44e0fc3428f6c76cc8af
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2014-02-15 13:16:43 -0800

    fixed facebook dev environment and added more photos

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   
   #live version
-  provider	:facebook, '1433798430185620', '552e358e083d5a177766969fbacba8ed'
+  #provider	:facebook, '1433798430185620', '552e358e083d5a177766969fbacba8ed'
 
   #development
   provider :facebook, '648784961845050', '18e14a9e34e3ca053332fb53c1a0d305'
