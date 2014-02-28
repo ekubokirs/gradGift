1df8da14ca99a27346cad160dd368d6cd6fc039a
Emilia Kirschenbaum <ekubokirs@gmail.com>
2014-02-28 11:35:35 -0800

    started styling to mocks

--- app/assets/javascripts/wishes.js
+++ app/assets/javascripts/wishes.js
@@ -4,27 +4,39 @@ $(function(){
 			$(".gw_newNice").hide();
 			$(".gw_newBlank").hide();
 
+			$("#gw_newSassy").removeClass("gw_inactive");
 			$("#gw_newSassy").addClass("gw_active");
+			
 			$("#gw_newNice").removeClass("gw_active");
 			$("#gw_newBlank").removeClass("gw_active");
+			$("#gw_newNice").addClass("gw_inactive");
+			$("#gw_newBlank").addClass("gw_inactive");
 	});
 	$("#gw_newNice").on("click", function(evt){
 			$(".gw_newSassy").hide();
 			$(".gw_newNice").show();
 			$(".gw_newBlank").hide();
 
-			$("#gw_newSassy").removeClass("gw_active");
+			$("#gw_newNice").removeClass("gw_inactive");
 			$("#gw_newNice").addClass("gw_active");
+
+			$("#gw_newSassy").removeClass("gw_active");
 			$("#gw_newBlank").removeClass("gw_active");
+			$("#gw_newSassy").addClass("gw_inactive");
+			$("#gw_newBlank").addClass("gw_inactive");
 	});
 	$("#gw_newBlank").on("click", function(evt){
 			$(".gw_newSassy").hide();
 			$(".gw_newNice").hide();
 			$(".gw_newBlank").show();
 
+			$("#gw_newBlank").removeClass("gw_inactive");
+			$("#gw_newBlank").addClass("gw_active");
+
 			$("#gw_newSassy").removeClass("gw_active");
 			$("#gw_newNice").removeClass("gw_active");
-			$("#gw_newBlank").addClass("gw_active");
+			$("#gw_newNice").addClass("gw_inactive");
+			$("#gw_newSassy").addClass("gw_inactive");
 	});
 	return false;
 });
